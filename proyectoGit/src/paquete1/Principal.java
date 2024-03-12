c976bd23ccdc1daf121ef9c885691a3bf59e924d
lbdan <lbdan@DLB>
2024-03-12 16:04:16 +0100

    Versión 4.0

--- proyectoGit/src/paquete1/Principal.java
+++ proyectoGit/src/paquete1/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("Probando repositorios Git en 1.º DAM\n");
-		System.out.println("Tercera prueba");
+		System.out.println("Tercerakjjg prueba");
 	}
 
 }
